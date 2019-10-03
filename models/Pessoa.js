b8bc46d85f6c5bbb550adde0f8d875cc322301d9
isasakis <isabellasakis@gmail.com>
2019-10-02 21:04:03 -0300

    create pessoa model

---
+++ models/Pessoa.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    cpf: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    endereco: {
+        cidade: {
+            type: String,
+            required: true
+        },
+        estado: {
+            type: String,
+            required: true
+        },
+        pais: {
+            type: String,
+            required: true
+        },
+        CEP: {
+            type: String,
+            required: true
+        },
+        bairro: {
+            type: String,
+            required: true
+        },
+        rua: {
+            type: String,
+            required: true
+        },
+        numero: {
+            type: Number,
+            required: true
+        },
+        complemento: {
+            type: String
+        }
+    }
+});
+
+module.exports = Pessoa = mongoose.model('pessoa', PessoaSchema);
